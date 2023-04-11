0715d0773e1691d7bc1bcea46c3517865fe82e9d
KimChansoo <kimchan1221@naver.com>
2023-04-11 11:20:14 +0900

    서버 포트변경 8080->8000 메인페이지 navigate 추가 로그인페이지 추가

--- src/main/java/com/toy/me/MainController.java
+++ src/main/java/com/toy/me/MainController.java
@@ -1,18 +1,35 @@
 package com.toy.me;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequiredArgsConstructor
+import java.util.HashMap;
+import java.util.Map;
+
+@CrossOrigin("*")
+@RestController
 public class MainController {
 
-    @RequestMapping("login")
+    @RequestMapping("/login")
     public String pageForwarding() {
         return "login";
+    }
+
+    @RequestMapping("/sessionCheck")
+    public Map<String, Object> sessCheck(HttpServletRequest request) {
+
+        Map<String, Object> result = new HashMap<>();
+
+        HttpSession session = request.getSession(false);
+        if(session == null) {
+            result.put("sessionCheck", true);
+        } else {
+            result.put("sessionCheck", false);
+        }
+
+        return result;
     }
 }
